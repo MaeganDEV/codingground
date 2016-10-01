c3fb1898e020ccfc95408232b70b6aa70534cfb1
MaeganDEV <maeganjefferson@gmail.com>
2016-10-01 14:22:45 -0400

    Update main.cpp
    control flow makes more sense, added setprecision to bmr

--- New Project-20160930/main.cpp
+++ New Project-20160930/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 //Calculates Male BMR
 double BMRmale(double wt, double ht, int age){
     double maleFormula = 66 + (6.23 * wt) + (12.7 * ht) - (6.8 * age);
-    cout << "Your BMR is " << maleFormula << endl;
+    cout << "Your BMR is " << fixed << setprecision(0) << maleFormula << endl;
     return 0;
 }
 //Calculates Female BMR
 double BMRfemale(double wt, double ht, int age){
     double femaleFormula = 655 + (4.35 * wt) + (4.7 * ht) - (6.8 * age);
-    cout << "Your BMR is " << femaleFormula << endl;
+    cout << "Your BMR is " << fixed << setprecision(0) << femaleFormula << endl;
     return 0;
 }
 
@@ -22,7 +23,7 @@ int main()
    int age = 0;
    double wt = 0.0;
    double ht = 0.0;
-   string gender = " ";
+   char gender = ' ';
 
     //io with calls to formula functions depending on condition
    cout << "Enter your Age: " ; 
@@ -33,14 +34,25 @@ int main()
    cin >> ht;
    cout << "Enter your Gender: (M/F): ";
    cin >> gender;
-    if (gender == "m" || "M" ){
+  
+    if (gender == 'M' || gender =='m'){
         BMRmale(wt,ht,age);
-    } else {
+    } else if (gender == 'F' || gender == 'f'){
         BMRfemale(wt,ht,age);
+    } else {
+        cout << "Try Again: Please enter 'M' for male or 'F' for female: ";
+        cin >> gender;
+            if (gender == 'M' || gender =='m'){
+                BMRmale(wt,ht,age);
+            } else if (gender == 'F' || gender == 'f'){
+                 BMRfemale(wt,ht,age);
+            }
     }
+
  
    
    return 0;
 }
+
 
 
